fa0ec6bb1a8f9e4118fb0457e0ab0f9412caf21b
Pranav aher <harshaher0404@gmail.com>
2020-09-30 20:22:46 +0530

    change navbar

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const hamburger_icon = document.querySelector('#navbar .navbar .nav-list .hamburger');
-const nav_list = document.querySelector('#navbar .navbar .nav-list');
+var nav_list = document.querySelector('#navbar .navbar .nav-list');
+var navbar = document.querySelector('.navbar')
 
 hamburger_icon.addEventListener('click',() => {
     nav_list.classList.toggle('open');
     hamburger_icon.classList.toggle('active');
+})
+
+window.addEventListener("scroll",() => {
+    if (window.pageYOffset >= 650){
+        navbar.style.transition = '2s';
+        navbar.style.background = 'rgb(167, 167, 167)';
+    } else{
+        navbar.style.background = 'transparent';
+    };
 })
